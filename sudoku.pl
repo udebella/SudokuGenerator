4ab26b5f8b29f50fd48e2c254c342a58c93a3961
Ubu <ubald2b@gmail.com>
2022-09-18 12:56:30 +0200

    lignes contiennent 9 valeurs

--- sudoku.pl
+++ sudoku.pl
@@ -1,9 +1,14 @@
-valide_ligne(ligne) :-
-    false.
+valide_ligne(LIGNE) :-
+    length(LIGNE, 9).
+     
 
+% swipl -g run_tests -t halt sudoku.pl
 :- begin_tests(sudoku).
 
 test('lignes contiennent des valeurs entre 1 et 9') :-
     not(valide_ligne([12])).
+
+test('lignes contiennent 9 valeurs') :-
+    valide_ligne([1, 2, 3, 4, 5, 6, 7, 8, 9]).
 
 :- end_tests(sudoku).
