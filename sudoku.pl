411bf3315ce1f0108e10f4a61d8dd977604dae9c
Ubu <ubald2b@gmail.com>
2022-09-18 15:20:59 +0200

    Can read columns from 1 length grid

--- sudoku.pl
+++ sudoku.pl
@@ -3,6 +3,8 @@
 valide_ligne(LIGNE) :-
     length(LIGNE, 9), LIGNE ins 1..9,
     all_distinct(LIGNE).
+
+read_columns_from_grid(X, X).
      
 
 % swipl -g run_tests -t halt sudoku.pl
@@ -19,5 +21,9 @@ test('une ligne contenant deux fois la même valeur est invalide') :-
 
 test('lignes contiennent 9 valeurs') :-
     valide_ligne([1, 2, 3, 4, 5, 6, 7, 8, 9]).
+
+
+test('can read columns from 1 length grids') :-
+    read_columns_from_grid([[1]], [[1]]).
 
 :- end_tests(sudoku).
