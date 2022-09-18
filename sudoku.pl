fca74f06adfa6a2e04728faf271cf2b4951ba9ec
Ubu <ubald2b@gmail.com>
2022-09-18 13:19:20 +0200

    une ligne contenant 10 est invalide

--- sudoku.pl
+++ sudoku.pl
@@ -1,12 +1,20 @@
+:- use_module(library(clpfd)).
+
+right_range([]).
+right_range([X | REST]):-
+    X in 1..9,
+    right_range(REST).
+
 valide_ligne(LIGNE) :-
-    length(LIGNE, 9).
+    length(LIGNE, 9),
+    right_range(LIGNE).
      
 
 % swipl -g run_tests -t halt sudoku.pl
 :- begin_tests(sudoku).
 
-test('lignes contiennent des valeurs entre 1 et 9') :-
-    not(valide_ligne([12])).
+test('une ligne contenant 10 est invalide') :-
+    not(valide_ligne([10, 1, 2, 3, 4, 5, 6, 7, 8])).
 
 test('lignes contiennent 9 valeurs') :-
     valide_ligne([1, 2, 3, 4, 5, 6, 7, 8, 9]).
